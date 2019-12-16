95b092385ae73e54b056cc972a00c3edb9190a41
harkon666 <51023310+harkon666@users.noreply.github.com>
2019-12-16 17:34:53 +0700

    Update index.js
    ZEZEZEZEZEZ dah sal

--- ROCKET (BE A DEVIL PROGRAMMER)/Find The Missing Letter/index.js
+++ ROCKET (BE A DEVIL PROGRAMMER)/Find The Missing Letter/index.js
@@ -1,6 +1,28 @@
 function findLetter(word) {
-  
+  const huruf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var result = []
 
+  for (var i = 0; i < word.length - 1; i++) {
+    var jekso = false
+    var count = 0
+      for (var j = 0; j < huruf.length; j++) {
+          if (huruf[j] === word[i]) {
+              jekso = true
+              continue
+          }
+          if (jekso === true) {
+              if (huruf[j] === word[i + 1]) {
+                  jekso = false
+                  if (count === 1) {
+                      result.push(huruf[j - 1])
+                  }
+              } else {
+                  count += 1
+              }
+          }
+      }
+  }
+  return result
 }
 
 
@@ -10,7 +32,7 @@ console.log(findLetter('ABDF')) // ['C', 'E']
 console.log(findLetter('OPQST')) // ['R']
 console.log(findLetter('DFGI')) // ['E', 'H']
 
-console.log("======================================================");
+console.log("=========================EZ=============================");
 
 // Release 1
 console.log(findLetter('GIAXZ')) // [ 'H', 'Y']
