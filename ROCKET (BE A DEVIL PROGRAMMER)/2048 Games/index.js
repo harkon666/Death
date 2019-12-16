2ba51ad5e6c76a22c889bc285efb338e68589342
harkon666 <51023310+harkon666@users.noreply.github.com>
2019-12-16 17:38:26 +0700

    udah kelar.. yang ini mayan lah

--- ROCKET (BE A DEVIL PROGRAMMER)/2048 Games/index.js
+++ ROCKET (BE A DEVIL PROGRAMMER)/2048 Games/index.js
@@ -1,18 +1,40 @@
 'use strict';
 // code below here
 function insertionSort(arr) {
-
-    
+    for (var i = 0; i < arr.length; i++) {
+        var temp = arr[i]
+        for (var j = i - 1; j >= 0; j--) {
+            if (arr[j] > temp) {
+                arr[j+1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
 
 }
 
 function aggregate(boards) {
-
-
+    var sort = insertionSort(boards)
+    var check = false
+    for (var i = sort.length - 1; i >= 0; i--) {
+        for (var j = i + 1; j < sort.length; j++) {
+            if (sort[j] === sort[i]) {
+                sort[i] += sort[j]
+                sort.splice(j,1)
+                check = true
+            }
+        }
+        if (check === true && i === 0) {
+            check = false
+            i = sort.length - 1
+        }
+    }
+    return sort
 
 }
 
-const boards = [1, 8, 1, 1, 1, 1, ]; // isi bebas
+const boards = [1,1,1,1,1,2,2,2,2,4,4,4,4,16,16,16,16,32,1,1,1,1,64,2,2,2,4,4,4,4,4,1,1,2 ]; // isi bebas
 
 // DRIVE CODE
 // Release 0 - insertionSort
